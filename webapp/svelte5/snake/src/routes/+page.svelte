d0537462344e5accd42c4b137b4121ebe60eb426
Alexander Kolberg <alexander.kolberg@alida.com>
2023-12-20 15:39:02 +0100

    add page

--- webapp/svelte5/snake/src/routes/+page.svelte
+++ webapp/svelte5/snake/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { SnakeGame } from '$lib/rpc.gen';
+	import { Game } from './game';
+
+	let canvas: HTMLCanvasElement;
+
+	const api = new SnakeGame('http://localhost:5252', fetch);
+
+	$effect(() => {
+		const game = new Game(canvas.getContext('2d')!, api);
+		game.start();
+	});
+</script>
+
+<div class="wrapper">
+	<canvas tabindex="1" bind:this={canvas} height={500} width={500}></canvas>
+</div>
+
+<style>
+	.wrapper {
+		background: -webkit-linear-gradient(top, #7fc5c9, #deabbe);
+		text-align: center;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	canvas {
+		margin: auto;
+	}
+</style>
